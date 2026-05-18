a9d324fcf7312942a446d68f2d9d002e723d34f5
Simon Whitlock <simonjwhitlock@gmail.com>
2026-05-18 20:48:41 +0100

    example .env file added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module govnotify_sender_SMS
 
 go 1.26
 
-require github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,39 +3,67 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
 )
 
-var key = flag.String("key", "", "Key Value")
+// define command line parameters.
 var phoneNumbers = flag.String("phoneNumbers", "", "Phone Numbers")
 var messageID = flag.String("messageID", "", "Message ID")
 var messageContent = flag.String("messageContent", "", "Message Content")
+var senderID = flag.String("senderID", "", "Sender ID")
 
 func main() {
+	//Load env file and read command line arguments.
+	godotenv.Load(".env")
 	flag.Parse()
-	fmt.Println(*key)
-	fmt.Println(*phoneNumbers)
-	fmt.Println(*messageID)
-	fmt.Println(*messageContent)
 
-	splitKey := strings.Split(*key, "-")
-	splitLen := len(splitKey)
-	iss := fmt.Sprintf("%s-%s-%s-%s-%s", splitKey[splitLen-10], splitKey[splitLen-9], splitKey[splitLen-8], splitKey[splitLen-7], splitKey[splitLen-6])
-	secret := fmt.Sprintf("%s-%s-%s-%s-%s", splitKey[splitLen-5], splitKey[splitLen-4], splitKey[splitLen-3], splitKey[splitLen-2], splitKey[splitLen-1])
-	fmt.Println("creating jwt")
-	token, err := createJWT(iss, secret)
+	// assign values from env
+	key := os.Getenv("API_KEY")
+	urlBase := os.Getenv("URL_BASE")
+	smsExt := os.Getenv("SMS_EXT")
+	callUrl := fmt.Sprintf("%s%s", urlBase, smsExt)
+
+	//split phone numbers into array
+	phoneNos := strings.Split(*phoneNumbers, ",")
+
+	//create token from key
+	token, tokenCreated, err := createJWT(key)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	fmt.Println(token)
+	fmt.Println(tokenCreated)
+	fmt.Println(*messageContent)
+
+	for _, phoneNo := range phoneNos {
+		if tokenCreated < (time.Now().Unix() - 25) {
+			token, tokenCreated, err = createJWT(key)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		body := fmt.Sprintf(`{"phone_number":"%s",template_id":"%s","personalisation": %s,"sms_sender_id": "%s"}`, phoneNo, *messageID, *messageContent, *senderID)
+		fmt.Println(callUrl)
+		fmt.Println(body)
+
+	}
 
 }
 
-func createJWT(iss, secretKey string) (string, error) {
+func createJWT(key string) (string, int64, error) {
+	//Split Iss and secret out of key.
+	splitKey := strings.Split(key, "-")
+	splitLen := len(splitKey)
+	iss := fmt.Sprintf("%s-%s-%s-%s-%s", splitKey[splitLen-10], splitKey[splitLen-9], splitKey[splitLen-8], splitKey[splitLen-7], splitKey[splitLen-6])
+	secret := fmt.Sprintf("%s-%s-%s-%s-%s", splitKey[splitLen-5], splitKey[splitLen-4], splitKey[splitLen-3], splitKey[splitLen-2], splitKey[splitLen-1])
 
+	//get time stamp (now!)
 	epoch := time.Now().Unix()
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
@@ -43,10 +71,10 @@ func createJWT(iss, secretKey string) (string, error) {
 		"iat": epoch,
 	})
 
-	signedToken, err := token.SignedString([]byte(secretKey))
+	signedToken, err := token.SignedString([]byte(secret))
 	if err != nil {
-		return "", err
+		return "", epoch, err
 	}
 
-	return signedToken, nil
+	return signedToken, epoch, nil
 }
